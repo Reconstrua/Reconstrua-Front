deb85f1948d7bc6edc7bd0eb6ebe7928023259d0
Oziel Sousa <ozielzin358@gmail.com>
2024-04-27 14:15:01 -0300

    refactor: second Section from Home page.

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function Button({ children, dark, disabled, buttonClass }) {
   return (
     <button className={twMerge(
-      "font-bold mt-8 rounded-2xl py-4 px-5 flex text-buttonClamp",
+      "font-bold mt-8 rounded-2xl py-4 px-5 flex",
       dark ? 'bg-component-light text-dark-text' : 'bg-component-dark text-dark-green',
       disabled ? "hidden" : null,
       buttonClass
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -9,10 +9,10 @@ export function Home() {
       <header className="sticky top-0 flex w-full justify-center py-4 bg-slate-700 bg-dark-green">
         <NavBar/>
       </header>
-      <section className="flex items-center justify-center md:gap-[129px] md:h-auto bg-dark-green w-full h-screen md:flex-col md:">
-        <div className="w-[50%] md:mt-6 h-full  md:h-auto md:items-center md:w-full flex flex-col justify-center items-end ">
+      <section className="flex md:gap-[109px] bg-dark-green w-full h-screen md:flex-col">
+        <div className="w-[50%] md:mt-6 h-full  md:items-center md:w-full flex flex-col justify-center items-end">
           <TextCard
-            className="flex flex-col gap-3 items-start w-[85%] "
+            className="flex justify-center flex-col gap-3 items-start h-full w-[85%] "
             title={
               <>
                 Conectando sonhos à <br /> realidade através da <br />{" "}
@@ -23,32 +23,34 @@ export function Home() {
             subtitle={<>Ajudamos pessoas em situação de vulnerabilidade <br /> social a realizar o sonho de ter sua própria casa. <br /> Fazemos isso conectando voluntários, doadores de <br /> materiais de construção e famílias necessitadas.</>}
             subtitleClass="md:text-lg text-subtitleClamp text-dark-text"
             buttonText="Torne-se um voluntário"
-            buttonClass="md:py-2 md:px-3 md:text-lg"
+            buttonClass="md:py-2 md:px-3 md:text-lg hover:bg-component-light hover:text-dark-text transform hover:scale-110 transition"
             disabled={false}
             dark={false}
           />
         </div>
-        <div className=" flex items-center justify-center w-[50%] md:w-full">
-          <img src={BlocksImage} alt="" className="w-[300px]" />
+        <div className=" flex items-center md:justify-center  h-full w-[50%] md:w-full">
+          <img src={BlocksImage} alt="" className="w-[85%] items-start" />
         </div>
       </section>
-      <section className="flex flex-row-reverse items-center justify-center bg-light-green w-full h-screen">
-        <div className="w-[50%] h-full flex flex-col justify-center items-end">
+      <section className="flex bg-light-green w-full h-screen md:flex-col">
+        <div className="flex w-[50%] h-full items-center justify-end md:justify-center md:w-full">
+          <img src={DonationImage} alt="" className="w-[85%] items-end flex" />
+        </div>
+        <div className="w-[50%] h-full  md:items-center md:w-full flex flex-col justify-center items-start ">
           <TextCard
-            className='flex flex-col gap-3 items-start w-[85%] pr-16'
-            title={'Quem somos'}
-            titleClass="text-titleClamp text-nowrap font-bold text-component-light"
-            subtitle={<>A Dream Home é mais do que uma organização, é um movimento impulsionado pela convicção de que todos merecem um lugar seguro para chamar de lar. Nós não apenas construímos casas, mas transformamos sonhos em realidades tangíveis, criando lares para pessoas em situação de vulnerabilidade social.<br></br>Trabalhamos em estreita colaboração com comunidades de baixa renda, voluntários dedicados e doadores de materiais de construção para tornar o sonho da casa própria uma realidade.</>}
-            subtitleClass="text-subtitleClamp text-dark-text"
-            buttonText={"Faça uma doação"}
+            className="flex justify-center flex-col gap-3 items-start h-full w-[85%]"
+            title="Quem somos"
+            titleClass="md:text-3xl text-titleClamp text-nowrap font-bold text-component-dark md:text-wrap"
+            subtitle={<>A Reconstrua é mais do que uma organização, é um movimento impulsionado pela convicção de que todos merecem um lugar seguro para chamar de lar. Nós não apenas construímos casas, mas transformamos sonhos em realidades tangíveis, criando lares para pessoas em situação de vulnerabilidade social.<br></br>Trabalhamos em estreita colaboração com comunidades de baixa renda, voluntários dedicados e doadores de materiais de construção para tornar o sonho da casa própria uma realidade.</>}
+            subtitleClass="md:text-lg text-subtitleClamp text-dark-text"
+            buttonText="Faça uma doação"
+            buttonClass="md:py-2 md:px-3 md:text-lg hover:bg-component-dark hover:text-dark-green transform hover:scale-110 transition"
             disabled={false}
-            dark
+            dark={true}
           />
         </div>
-        <div className="flex items-center justify-center w-[50%]">
-          <img src={DonationImage} alt="" className="w-[500px]" />
-        </div>
       </section>
+     
     </>
   );
 }
