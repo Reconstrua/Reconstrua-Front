f06c271240e731399681aa39e17aa87c480469af
João Targino <joaoetargino@gmail.com>
2024-05-13 17:14:42 -0300

    style: removed unused functions on ToggleModalContext

--- src/context/ToggleModalContext.jsx
+++ src/context/ToggleModalContext.jsx
@@ -1,21 +1,17 @@
 import { createContext, useState } from "react";
-// import { getDataById } from "../utils/apiFunctions"
-
-
 
 export const ToggleModalContext = createContext();
 
 export const ToggleModalProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false)
-  const [data, setData] = useState([])
 
-  const toggleModal = (dataType, dataId) => {
-      // getDataById(dataType,dataId,setData);
+  const toggleModal = () => {
+
     setIsOpen(!isOpen)
   }
 
   return (
-    <ToggleModalContext.Provider value={{ isOpen, toggleModal, data }}>
+    <ToggleModalContext.Provider value={{ isOpen, toggleModal }}>
       {children}
     </ToggleModalContext.Provider>
   )
