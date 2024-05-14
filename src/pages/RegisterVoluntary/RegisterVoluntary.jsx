4a34a73447b88e95aaa38c3b0b4aeec7f9b38084
João Targino <joaoetargino@gmail.com>
2024-05-14 18:32:27 -0300

    refactor: updated all register screens with new forms components

--- src/pages/RegisterVoluntary/RegisterVoluntary.jsx
+++ src/pages/RegisterVoluntary/RegisterVoluntary.jsx
@@ -1,43 +1,13 @@
-import { DoubleInput } from "../../components/DoubleInput"
-import { LargeInput } from "../../components/LargeInput"
 import { Header } from "../../components/Header"
-import { SingleInput } from "../../components/SingleInput"
 import { TextCard } from "../../components/TextCard"
 import Builders from "../../assets/builders.svg"
-import { Button } from "../../components/Button"
 import { Footer } from "../../components/Footer/Footer"
-import { AxiosAPI } from "../../AxiosConfig"
-import { useState } from "react"
-import { toast } from 'react-toastify';
+// import { AxiosAPI } from "../../AxiosConfig"
+// import { useState } from "react"
+// import { toast } from 'react-toastify';
+import { VoluntaryForm } from "../../components/Forms/VoluntaryForm"
 
 export function RegisterVoluntary() {
-    const [firstName,setFirstName] = useState("")
-    const [lastName,setLastName] = useState("")
-    const [email,setEmail] = useState("")
-    const [phone,setPhone] = useState("")
-    const [description,setDescription] = useState("")
-
-    async function postVoluntary() {
-        try {
-            toast.promise(
-                AxiosAPI.post("/voluntaries", {
-                    first_name: `${firstName}`,
-                    last_name: `${lastName}`,
-                    email: `${email}`,
-                    phone: `${phone}`,
-                    description: `${description}`
-                }),
-                {
-                    pending: 'Enviando...',
-                    success: 'Enviado com Sucesso!',
-                    error: 'Erro ao enviar'
-                }
-            );
-        } catch (err) {
-            console.error(err);
-            toast.error('Erro ao enviar');
-        }
-    }
 
     return (
         <>
@@ -55,15 +25,7 @@ export function RegisterVoluntary() {
                     <img src={Builders} alt="" className="w-3/4 mx-0 sm:w-full mx-auto" />
                 </div>
 
-                <form className="w-1/2 sm:w-full p-16">
-                    <div className="grid gap-6 mb-6 ">
-                        <DoubleInput onChangeHandleFirst={(e)=> setFirstName(e.target.value)} onChangeHandleSecond={(e)=> setLastName(e.target.value)} text1="Nome" placeholder1="Digite seu nome" text2="Sobrenome" placeholder2="Digite seu sobrenome" />
-                        <SingleInput onChange={(e)=> setEmail(e.target.value)} text="E-Mail" placeholder="Digite seu endereço de e-mail" />
-                        <SingleInput onChange={(e)=> setPhone(e.target.value)} text="Celular" placeholder="XX XXXXX-XXXX" />
-                        <LargeInput onChange={(e)=> setDescription(e.target.value)} text="Por que você quer ser um voluntário?" />
-                        <Button buttonClass="w-full" onClick={postVoluntary} >Me tornar um voluntário</Button>
-                    </div>
-                </form>
+                <VoluntaryForm method="post" />
             </section >
             <Footer />
         </>
