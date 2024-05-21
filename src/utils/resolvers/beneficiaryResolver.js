92b28e6b946047a859119986b89c784cc9e5953c
João Targino <joaoetargino@gmail.com>
2024-05-21 19:41:40 -0300

    feat: implemented zod in the beneificiary forms validation

---
+++ src/utils/resolvers/beneficiaryResolver.js
@@ -0,0 +1,45 @@
+import { z } from "zod";
+
+const phoneRegex = /^(\+?55)?\s?\(?\d{2}\)?[\s-]?\d{4,5}[\s-]?\d{4}$/;
+const cepRegex = /^\d{5}-\d{3}$/;
+
+export const beneficiarySchema = z.object({
+    first_name: z.string({ message: "O nome deve ser um texto válido!" }).nonempty({ message: "O nome é obrigatório!" }),
+
+    last_name: z.string({ message: "O sobrenome deve ser um texto válido!" }).nonempty({ message: "O sobrenome é obrigatório!" }),
+
+    email: 
+    z.string()
+    .nonempty({ message: "O e-mail é obrigatório!" })
+    .email({ message: "Por favor, insira um endereço de email válido" }),
+
+    phone: 
+    z.string()
+    .nonempty({ message: "O telefone é obrigatório!" })
+    .regex(phoneRegex, {message: "Por favor, insira um telefone válido!"}),
+
+    cep: 
+    z.string()
+    .nonempty({ message: "O CEP é obrigatório!" })
+    .regex(cepRegex, { message: "CEP inválido. Deve ser no formato 00000-000" }),
+
+    street: z.string().nonempty({ message: "A rua é obrigatória!" }),
+
+    neighborhood: z.string().nonempty({ message: "O bairro é obrigatório!" }),
+
+    city: 
+    z.string()
+    .nonempty({ message: "A cidade é obrigatória!" })
+    .min(3, { message: "A cidade deve ter pelo menos 3 caracteres" }),
+
+    state: z.string()
+    .nonempty({ message: "O estado é obrigatório!" })
+    .min(2, { message: "O estado deve ter pelo menos 2 caracteres" }),
+
+    address_number: 
+    z.number({message: "O número da casa deve ser um número!"})
+    .int({message: "O número da casa deve ser um número inteiro!"})
+    .nonnegative({message: "O número da casa deve ser um número positivo!"}),
+
+    description: z.string().nonempty({ message: "A descrição é obrigatória!" })
+});
