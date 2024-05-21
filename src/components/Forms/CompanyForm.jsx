79742db43ef6b92d9c15bac661a50c1459df8903
João Targino <joaoetargino@gmail.com>
2024-05-21 20:04:30 -0300

    feat: implemented zod in the company forms validation

--- src/components/Forms/CompanyForm.jsx
+++ src/components/Forms/CompanyForm.jsx
@@ -1,8 +1,10 @@
 import { useForm } from "react-hook-form"
 import { getDataById, postData, updateData } from "../../utils/apiFunctions";
 import { Button } from "../Button";
-import { ToggleModalContext } from "../../contexts/ToggleModalContext";
 import { useContext, useEffect } from "react";
+import { ToggleModalContext } from "../../contexts/ToggleModalContext";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { companySchema } from "../../utils/resolvers/companyResolver";
 
 export function CompanyForm({ method, id }) {
 
@@ -13,7 +15,7 @@ export function CompanyForm({ method, id }) {
         handleSubmit,
         setValue,
         formState: { errors }
-    } = useForm();
+    } = useForm({resolver: zodResolver(companySchema)});
 
     let onSubmit;
 
@@ -57,46 +59,28 @@ export function CompanyForm({ method, id }) {
                 <div>
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">Nome da sua empresa</label>
                     <input type="text" id="company_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="Digite o nome da sua empresa"
-                        {...register("company_name", {required: "O nome é obrigatório!",})}
+                        {...register("company_name")}
                     />
                     {errors?.company_name && <p className="text-[#ff1e1e]">{errors.company_name.message}</p>}
                 </div>
                 <div>
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">E-Mail</label>
                     <input type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="Digite o email da empresa"
-                        {...register("email", {
-                            required: "O e-mail é obrigatório!",
-                            pattern: {
-                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                message: "Por favor, insira um endereço de email válido",
-                            },
-                        })}
+                        {...register("email")}
                     />
                     {errors?.email && <p className="text-[#ff1e1e]">{errors.email.message}</p>}
                 </div>
                 <div>
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">Celular</label>
                     <input type="text" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="XX XXXXX-XXXX"
-                        {...register("phone", {
-                            required: "O telefone é obrigatório!",
-                            pattern: {
-                                value: /^\d{11}$/,
-                                message: "Por favor, insira um número de telefone válido",
-                            },
-                        })}
+                        {...register("phone")}
                     />
                     {errors?.phone && <p className="text-[#ff1e1e]">{errors.phone.message}</p>}
                 </div>
                 <div>
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">Site</label>
                     <input type="text" id="website" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="https://seusite.com.br"
-                        {...register("website", {
-                            required: "O site é obrigatório!",
-                            pattern: {
-                                value: /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
-                                message: "Por favor, insira um site válido!",
-                            },
-                        })}
+                        {...register("website")}
                     />
                     {errors?.website && <p className="text-[#ff1e1e]">{errors.website.message}</p>}
                 </div>
@@ -104,20 +88,14 @@ export function CompanyForm({ method, id }) {
                     <div className="w-1/2">
                         <label htmlFor="cep" className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">CEP</label>
                         <input type="text" id="cep" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="Digite seu CEP"
-                            {...register("cep", {
-                                required: "CEP é obrigatório",
-                                pattern: {
-                                    value: /^\d{5}-\d{3}$/,
-                                    message: "CEP inválido. Deve ser no formato 00000-000",
-                                },
-                            })}
+                            {...register("cep")}
                         />
                         {errors?.cep && <p className="text-[#ff1e1e]">{errors.cep.message}</p>}
                     </div>
                     <div className="w-1/2">
                         <label htmlFor="street" className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">Rua</label>
                         <input type="text" id="street" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="Digite sua rua"
-                            {...register("street", { required: "A rua é obrigatória!" })}
+                            {...register("street")}
                         />
                         {errors?.street && <p className="text-[#ff1e1e]">{errors.street.message}</p>}
                     </div>
@@ -126,20 +104,14 @@ export function CompanyForm({ method, id }) {
                     <div className="w-1/2">
                         <label htmlFor="neighborhood" className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">Bairro</label>
                         <input type="text" id="neighborhood" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="Digite seu bairro"
-                            {...register("neighborhood", { required: "O bairro é obrigatório!" })}
+                            {...register("neighborhood")}
                         />
                         {errors?.neighborhood && <p className="text-[#ff1e1e]">{errors.neighborhood.message}</p>}
                     </div>
                     <div className="w-1/2">
                         <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">Cidade</label>
                         <input type="text" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="Digite sua Cidade"
-                            {...register("city", {
-                                required: "A cidade é obrigatória!",
-                                minLength: {
-                                    value: 3,
-                                    message: "A cidade deve ter pelo menos 3 caracteres",
-                                },
-                            })}
+                            {...register("city")}
                         />
                         {errors?.city && <p className="text-[#ff1e1e]">{errors.city.message}</p>}
                     </div>
@@ -148,20 +120,14 @@ export function CompanyForm({ method, id }) {
                     <div className="w-1/2">
                         <label htmlFor="state" className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">Estado</label>
                         <input type="text" id="state" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="Digite seu estado"
-                            {...register("state", {
-                                required: "O estado é obrigatório!",
-                                minLength: {
-                                    value: 3,
-                                    message: "O estado deve ter pelo menos 3 caracteres",
-                                },
-                            })}
+                            {...register("state")}
                         />
                         {errors?.state && <p className="text-[#ff1e1e]">{errors.state.message}</p>}
                     </div>
                     <div className="w-1/2">
                         <label htmlFor="address_number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">Número da casa</label>
                         <input type="text" id="address_number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text" placeholder="Digite o número da casa"
-                            {...register("address_number", { required: "O número é obrigatório!" })}
+                            {...register("address_number")}
                         />
                         {errors?.address_number && <p className="text-[#ff1e1e]">{errors.address_number.message}</p>}
                     </div>
@@ -169,13 +135,7 @@ export function CompanyForm({ method, id }) {
                 <div className="mb-6">
                     <label htmlFor="large-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-component-light">Conte-nos o que sua empresa faz</label>
                     <input type="text" id="large-input" className="block w-full p-12 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-light-green dark:border-gray-600 dark:placeholder-light-text dark:text-light-text"
-                        {...register("description", {
-                            required: "A descrição é obrigatória!",
-                            minLength: {
-                                value: 10,
-                                message: "A descrição deve ter pelo menos 10 caracteres",
-                            },
-                        })}
+                        {...register("description")}
                     />
                     {errors?.description && <p className="text-[#ff1e1e]">{errors.description.message}</p>}
                 </div>
